694b262c0a6b3b3e9ea380ede88c87671d7948cb
Hoang Dung <dungnguyen322003@gmail.com>
2023-03-11 21:41:03 +0700

    Complete Navbar, Sidebar and API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography, Box, Stack } from '@mui/material';
+
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet,id&q=${selectedCategory}`).then(
+      (data) => {
+        setVideos(data.items);
+      }
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2023 Hoang Dung
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          {selectedCategory} <span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
