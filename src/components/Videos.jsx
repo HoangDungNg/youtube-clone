c0e741e9407fb017f10d6eefa511585870172f1c
Hoang Dung <dungnguyen322003@gmail.com>
2023-03-13 21:48:13 +0700

    Complete ChannelCard, VideoCard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
 
 const Videos = ({ videos }) => {
-  console.log(videos);
+  //console.log(videos);
 
-  return <div>Videos</div>;
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      gap={2}
+      sx={{ justifyContent: { xs: 'center', md: 'start' } }}
+    >
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {(item?.id?.videoId || item?.id?.playlistId) && (
+            <VideoCard video={item} />
+          )}
+          {item?.id?.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
